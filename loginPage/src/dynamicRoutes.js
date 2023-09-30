e8dabb4cb990e5781c87a04f273d1d8decc813e7
Yadvendra Sharma <yadvendras20@gmail.com>
2023-09-30 19:57:24 +0530

    m

--- loginPage/src/dynamicRoutes.js
+++ loginPage/src/dynamicRoutes.js
@@ -1,5 +1,5 @@
 // environmentVariables.js
-const API_URL = "http://65.1.54.22:8000"; 
+const API_URL = "https://65.1.54.22:8000"; 
 const SOCKET_API_URL = "ws://65.1.54.22:7000"; 
 // const API_URL = "https://degen-scalper.onrender.com"; 
 // const SOCKET_API_URL = "ws://localhost:7000"; 
